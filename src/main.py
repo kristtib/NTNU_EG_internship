a0780ba68e9f276a6a05190210cde0d527ef9dc0
Morten Fredriksen <morfre@equinor.com>
2024-01-25 12:54:29 +0100

    Plot loading percentage

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
 import pandapower as pp
+import matplotlib.pyplot as plt
+
+from cable_options import add_cables_to_std_library
 
 network = pp.create_empty_network(f_hz=50)
+add_cables_to_std_library(network)
 bus1 = pp.create_bus(
     network,
     name="bus1",
-    vn_kv=66,
+    vn_kv=100,
     type="b",
 ).item()
 bus2 = pp.create_bus(
@@ -13,33 +17,40 @@ bus2 = pp.create_bus(
     vn_kv=230,
     type="b",
 ).item()
-gen1 = pp.create_sgen(net=network, bus=bus1, p_mw=50)
+gen1 = pp.create_sgen(net=network, bus=bus1, p_mw=300)
 
-trafo1 = pp.create_transformer(net=network, lv_bus=bus1, hv_bus=bus2, std_type="100 MVA 220/110 kV")
+trafo1 = pp.create_transformer(
+    net=network, lv_bus=bus1, hv_bus=bus2, std_type="100 MVA 220/110 kV"
+)
 
 
-for i in range (0,3):
+for i in range(0, 8):
     pp.create_bus(
         network,
-        name="bus"+str(3+i),
+        name="bus" + str(3 + i),
         vn_kv=230,
         type="b",
     ).item()
 
     pp.create_line(
         net=network,
-        from_bus=1+i,
-        to_bus=2+i,
-        std_type="490-AL1/64-ST1A 110.0",
+        name=f"line{i}",
+        from_bus=1 + i,
+        to_bus=2 + i,
+        std_type="245kv_1000mm2_cu",
+        # std_type="490-AL1/64-ST1A 110.0",
         length_km=10,
     )
 
 
-pp.create_ext_grid(net=network, bus=4)
+pp.create_ext_grid(net=network, bus=9)
 pp.runpp(net=network, algorithm="nr", run_control=True, numba=True)
 
 pp.plotting.to_html(net=network, filename="test.html")
 
-network.res_line.plot(y="i_ka",kind="line")
 
+line_names = network.line["name"]
+loading_percentages = network.res_line["loading_percent"]
+plt.plot(line_names, loading_percentages)
+plt.show()
 print(network)
