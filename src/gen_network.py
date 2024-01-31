7ff0e574b22c597dad0668c8c7338d7fa9a3c7ae
marieler <145976684+marieler@users.noreply.github.com>
2024-01-31 14:06:26 +0100

    Add function p_q_plot, changes to line_current_plot to include multiple q values

--- src/gen_network.py
+++ src/gen_network.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from cable_options import add_cables_to_std_library
 from trafo_options import add_trafos_to_std_library
 
-def generate_basic_network():
+def generate_basic_network(sgen_p= float, sgen_q = float, total_line_length = float, per_line_length = float):
     network = pp.create_empty_network(f_hz=50)
     add_cables_to_std_library(network)
     add_trafos_to_std_library(network)
@@ -22,13 +22,11 @@ def generate_basic_network():
         type="b",
     ).item()
 
-    pp.create_sgen(net=network, bus=0, p_mw=400,q_mvar=-75)
+    pp.create_sgen(net=network, bus=0, p_mw=sgen_p,q_mvar=sgen_q)
     pp.create_transformer(
         net=network, lv_bus=0, hv_bus=1, std_type="OFS_tx_220kV_66kV_400MVA"
     )
 
-    total_line_length = 80 #km
-    per_line_length = 1
     for i in range(0, total_line_length, per_line_length):
         pp.create_bus(
             network,
@@ -63,12 +61,43 @@ def generate_basic_network():
 
     return network
 
-def line_current_plot(network):
-    line_names = network.line["name"]
-    line_currents = network.res_line["i_ka"]
-    plt.plot(line_names, line_currents)
-    plt.show()
+def line_current_plot(sgen_p= float, sgen_q = list, total_line_length = float, per_line_length = float):
+    plt.figure()
+    plt.xlabel('line length [km]')
+    plt.ylabel('I [kA]')
+    for q in sgen_q:
+        network = generate_basic_network(sgen_p=sgen_p, sgen_q=q, total_line_length=total_line_length, per_line_length=per_line_length)
+        
+        line_names = network.line["name"]
+        line_currents = network.res_line["i_ka"]
+        
+        plt.plot(line_names, line_currents, label=f"Q={q}[MVAr]")
+    plt.legend(loc="upper right")
+    plt.grid()
+    
+    
      
+def p_q_plot(sgen_p= float, sgen_q = list, total_line_length = float, per_line_length = float):
+    plt.figure()
+    plt.xlabel('Q_sys [MVAr]')
+    plt.ylabel('P [MW]')
 
-n = generate_basic_network()
-line_current_plot(n)
+    for q in sgen_q:
+        active_powers = []
+        reactive_powers = []
+        step_p = int(sgen_p/10)
+        for i in range (0,sgen_p+step_p,step_p):
+            network = generate_basic_network(sgen_p=i, sgen_q=q, total_line_length=total_line_length, per_line_length=per_line_length)
+            active_powers.append(i)
+            reactive_powers.append(network.res_bus.iloc[-1].q_mvar - q)
+
+        plt.plot(reactive_powers, active_powers, label=f"Q={q}[MVAr]")
+    
+    plt.legend(loc="upper right")
+    plt.grid()
+    plt.show()
+
+list_of_q = range(-100,-50,10)
+line_current_plot(400,list_of_q,80,1)
+p_q_plot(400,list_of_q,80,1)
+#n = generate_basic_network()
