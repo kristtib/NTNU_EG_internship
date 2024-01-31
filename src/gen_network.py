781820b40913c2683430b442409994aed254e972
marieler <145976684+marieler@users.noreply.github.com>
2024-01-31 12:32:28 +0100

    Made changes to gen_network.py after new file trafo_options.py

--- src/gen_network.py
+++ src/gen_network.py
@@ -2,50 +2,64 @@ import pandapower as pp
 import numpy as np
 import matplotlib.pyplot as plt
 from cable_options import add_cables_to_std_library
+from trafo_options import add_trafos_to_std_library
 
 def generate_basic_network():
     network = pp.create_empty_network(f_hz=50)
     add_cables_to_std_library(network)
-    bus1 = pp.create_bus(
+    add_trafos_to_std_library(network)
+
+    pp.create_bus(
         network,
         name="bus1",
-        vn_kv=100,
+        vn_kv=66,
         type="b",
     ).item()
-    bus2 = pp.create_bus(
+    pp.create_bus(
         network,
         name="bus2",
         vn_kv=230,
         type="b",
     ).item()
 
-    gen1 = pp.create_sgen(net=network, bus=bus1, p_mw=300,q_mvar=-10)
-    trafo1 = pp.create_transformer(
-        net=network, lv_bus=bus1, hv_bus=bus2, std_type="100 MVA 220/110 kV"
+    pp.create_sgen(net=network, bus=0, p_mw=400,q_mvar=-75)
+    pp.create_transformer(
+        net=network, lv_bus=0, hv_bus=1, std_type="OFS_tx_220kV_66kV_400MVA"
     )
 
-    for i in range(0, 8):
+    total_line_length = 80 #km
+    per_line_length = 1
+    for i in range(0, total_line_length, per_line_length):
         pp.create_bus(
             network,
-            name="bus" + str(network.bus.iloc[-1].name + 2),
+            name="bus"+str(network.bus.iloc[-1].name+2),
             vn_kv=230,
             type="b",
         ).item()
 
         pp.create_line(
             net=network,
-            name=f"line{i}",
+            name=f"line{i+per_line_length}",
             from_bus=network.bus.iloc[-2].name,
             to_bus=network.bus.iloc[-1].name,
             std_type="245kv_1000mm2_cu",
             # std_type="490-AL1/64-ST1A 110.0",
-            length_km=10,
+            length_km=per_line_length,
         )
     
-    
+    pp.create_bus(
+            network,
+            name="bus"+str(network.bus.iloc[-1].name+2),
+            vn_kv=400,
+            type="b",
+        ).item()
+    pp.create_transformer(
+        net=network, lv_bus=network.bus.iloc[-2].name, hv_bus=network.bus.iloc[-1].name, std_type="ONS_tx_400kV_230kV_400MVA"
+    )
 
     pp.create_ext_grid(net=network, bus=network.bus.iloc[-1].name)
     pp.runpp(net=network, algorithm="nr", run_control=True, numba=True)
+    pp.plotting.to_html(net=network, filename="test.html")
 
     return network
 
@@ -54,7 +68,7 @@ def line_current_plot(network):
     line_currents = network.res_line["i_ka"]
     plt.plot(line_names, line_currents)
     plt.show()
-    
+     
 
 n = generate_basic_network()
 line_current_plot(n)
