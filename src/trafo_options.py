651f9f063ea9624c4536dfede66291e03715e902
marieler <145976684+marieler@users.noreply.github.com>
2024-02-21 10:47:19 +0100

    Removed trafo3w from trafo_options

--- src/trafo_options.py
+++ src/trafo_options.py
@@ -61,31 +61,7 @@ def add_trafos_to_std_library(net):
                                 "vector_group": "YNd11",    
                                 "tap_side": "hv",   
                                 "tap_neutral": 0,                                    
-                                "tap_min": -10, "tap_max": 10,  "tap_step_percent": 1.25},
-
-                            {"name" : "on_tx_400kV_220kV_36kV_450MVA", 
-                             "sn_hv_mva": 450, 
-                             "sn_mv_mva": 380, 
-                             "sn_lv_mva": 250, 
-                             "vn_hv_kv": 410, 
-                             "vn_mv_kv": 230, 
-                             "vn_lv_kv": 35.7, 
-                             "vk_hv_percent": 13,
-                                     "vk_mv_percent": 15, 
-                                     "vk_lv_percent": 18, 
-                                     "vkr_hv_percent": 0.17, 
-                                     "vkr_mv_percent": 0.35, 
-                                     "vkr_lv_percent": 0.44, 
-                                     "pfe_kw": 114 ,
-                                     "i0_percent": 0.2, 
-                                     "shift_mv_degree": 0, 
-                                     "shift_lv_degree": 0, 
-                                     "vector_group": "YNyn0d11", 
-                                     "tap_side": "hv",   
-                                     "tap_neutral": 0,
-                                     "tap_min": -10,    
-                                     "tap_max": 10,  
-                                     "tap_step_percent": 1.5 }    
+                                "tap_min": -10, "tap_max": 10,  "tap_step_percent": 1.25}    
                 ]
     for trafo in trafos:
         add_trafo_to_pp_net_library(net, trafo)
